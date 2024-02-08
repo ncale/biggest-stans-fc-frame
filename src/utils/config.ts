dd1bd267a45df23a8e96ae3031a53f6b6cd5944a
ncale <nicholasbrodeur@outlook.com>
2024-02-08 11:34:53 -0600

    fix relative path for font

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -4,4 +4,4 @@ dotenv.config();
 export const NEXT_PUBLIC_URL = 'https://biggest-stans-fc-frame-production.up.railway.app/';
 export const DUNE_API_KEY = process.env.DUNE_API_KEY!;
 export const DUNE_QUERY_ID = 3411798;
-export const SVG_FONT_URL = '../../assets/Inter.ttf';
+export const SVG_FONT_URL = 'biggest-stans-fc-frame/assets/inter.ttf';
